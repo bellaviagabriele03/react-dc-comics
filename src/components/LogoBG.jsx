8bf2fa175fc496ea212c0863d94e14c1694eabb2
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-11-25 18:21:10 +0100

    Fix: container aggiunto agli elementi senza inttaccare gli sfondi, aggiunti gli anchor tag nella nav bar del footer wip: footer bottom, e regolazione LogoBG del footer

---
+++ src/components/LogoBG.jsx
@@ -0,0 +1,7 @@
+import imgLogoBG from "../assets/img/dc-logo-bg.png"
+
+export default function LogoBG () {
+    return (
+        <img src={imgLogoBG} alt="" />
+    )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,14 @@
 import style from "./Main.module.css"
 
 const string = "--> Content Goes Here <--"
-const className = `container py-40 ${style.background} ${style.text}`
+const className = `py-40 ${style.background} ${style.text}`
 
 export default function Main() {
     return (
         <main className={className}>
-           {string}
+            <div className="container">
+                <h1>{string}</h1>
+            </div>
         </main>
     )
 }
