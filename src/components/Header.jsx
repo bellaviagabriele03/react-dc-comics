ee30978a4fec0fddd6d5dc225034f2e91df921e4
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-11-26 09:48:55 +0100

    Feat: aggiunti link nel header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,22 +2,24 @@ import Logo from "./Logo"
 
 export default function Header() {
     return (
-        <header className="d-flex justify-content-around py-40 align-items-center">
+        <header className="d-flex py-40 align-items-center justify-content-around">
+
             <Logo />
             <nav>
                 <ul className="d-flex gap-20">
-                    <li>link</li>
-                    <li>link</li>
-                    <li>link</li>
-                    <li>link</li>
-                    <li>link</li>
-                    <li>link</li>
-                    <li>link</li>
-                    <li>link</li>
-                    <li>link</li>
-                    <li>link</li>
+                    <li><a href="">CHARACTERS</a></li>
+                    <li><a href="">COMICS</a></li>
+                    <li><a href="">MOVIES</a></li>
+                    <li><a href="">TV</a></li>
+                    <li><a href="">GAMES</a></li>
+                    <li><a href="">COLLECTIBLES</a></li>
+                    <li><a href="">VIDEOS</a></li>
+                    <li><a href="">FANS</a></li>
+                    <li><a href="">NEWS</a></li>
+                    <li><a href="">SHOP</a></li>
                 </ul>
             </nav>
+
         </header>
 
     )
